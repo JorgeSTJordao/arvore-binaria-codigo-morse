2d3707539f745bdf42b2b56f7378c685fd93d26c
Jorge Samuel Teixeira Jordão <116199476+JorgeSTJordao@users.noreply.github.com>
2024-09-27 11:43:07 -0300

    :new: Explained what each method does

--- src/arvore/binaria/codigo/morse/Estrutura.java
+++ src/arvore/binaria/codigo/morse/Estrutura.java
@@ -5,22 +5,27 @@ public class Estrutura {
     private String caractere;
     private String simbolo;
     
+    //Estrutura de dados do nó
     public Estrutura(String simbolo){
         this.simbolo = simbolo;
     }
     
+    //Retorna o caractere
     public String getCaratere(){
         return caractere;
     }
     
+    //Altera o caractere
     public void setCaractere(String caractere){
         this.caractere = caractere;
     }
     
-        public String getSimbolo(){
+    //Retorna o código 
+    public String getSimbolo(){
         return simbolo;
     }
     
+    //Altera o código
     public void setSimbolo(String simbolo){
         this.simbolo = simbolo;
     }
--- src/arvore/binaria/codigo/morse/Node.java
+++ src/arvore/binaria/codigo/morse/Node.java
@@ -6,39 +6,49 @@ public class Node {
     private Node esquerdo;
     private Node direito;
     
+    //Nó com a estrutura (caractere e código morse, subárvore a esquerda, subárvore a direita)
     public Node(String simbolo){
         this.estrutura = new Estrutura(simbolo);
         this.esquerdo = null;
         this.direito = null;
     }
     
+    //Altera o caractere
     public void setEstruturaCaractere(String caractere){
         estrutura.setCaractere(caractere);    
     }
     
+    //Retorna o caractere
     public String getEstruturaCaractere(){
         return estrutura.getCaratere();    
     }
     
+    //Altera o código
     public void setEstruturaSimbolo(String simbolo){
         estrutura.setSimbolo(simbolo);    
     }
     
+    //Retorna o código
     public String getEstruturaSimbolo(){
         return estrutura.getSimbolo();    
     }
     
+    //Retorna a subárvore da esquerda
     public Node getEsquerda(){
         return esquerdo;
     }
     
+    //Altera a subárvore da esquerda
     public void setEsquerda(Node node){
         esquerdo = node;
     }
     
+    //Retorna a subárvore da direita   
     public Node getDireita(){
         return direito;
     }
+    
+    //Altera a subárvore da direita
     public void setDireita(Node node){
         direito = node;
     }
